b3aa9bb047c7d8ad7a8c7f40bd3c9f3178f40337
HoussemHaddad <haddadhouss2019@gmail.com>
2025-08-17 19:28:21 +0100

    -m

---
+++ tunisie-telecom-backend/controllers/authController.js
@@ -0,0 +1,67 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const { promisify } = require('util');
+
+const signToken = id => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN
+  });
+};
+
+exports.register = async (req, res, next) => {
+  try {
+    const newUser = await User.create({
+      email: req.body.email,
+      password: req.body.password,
+      role: req.body.role
+    });
+
+    const token = signToken(newUser._id);
+
+    res.status(201).json({
+      status: 'success',
+      token,
+      data: {
+        user: newUser
+      }
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err.message
+    });
+  }
+};
+
+exports.login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    // 1) Check if email and password exist
+    if (!email || !password) {
+      throw new Error('Please provide email and password!');
+    }
+
+    // 2) Check if user exists && password is correct
+    const user = await User.findOne({ email }).select('+password');
+    if (!user || !(await user.comparePassword(password))) {
+      throw new Error('Incorrect email or password');
+    }
+
+    // 3) If everything ok, send token to client
+    const token = signToken(user._id);
+
+    res.status(200).json({
+      status: 'success',
+      token,
+      data: {
+        user
+      }
+    });
+  } catch (err) {
+    res.status(401).json({
+      status: 'fail',
+      message: err.message
+    });
+  }
+};
